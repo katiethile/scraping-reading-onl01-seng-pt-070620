f72ac9ce33af1f1cd16fabcb42296c6ddfe6f069
Katie Le <lethikatie@gmail.com>
2020-09-04 02:53:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,6 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
-
-require 'nokogiri'
-require 'open-uri'
  
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
